e812f367eefcc4f5f3ce8fcfbf531c793c24237c
Crow314 <master@crow31415.net>
2022-07-19 14:36:42 +0900

    Week02 f1

---
+++ 02/f1.ml
@@ -0,0 +1,14 @@
+let max2 a b =
+  if a > b then
+    a
+  else
+    b;;
+
+let rec solve list1 max_val =
+  match list1 with
+  | [] -> max_val
+  | h::t -> solve t (max2 h max_val);;
+
+let f1 list1 = 
+  let max = 0 in
+  solve list1 max;
