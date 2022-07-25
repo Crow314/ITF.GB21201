0a38235e669930df61f9b3f64fef8836ef0e096a
Crow314 <master@crow31415.net>
2022-07-25 15:50:03 +0900

    Update Week01 f3

--- 01/f3.ml
+++ 01/f3.ml
@@ -1,15 +1,8 @@
-let rec pow x n =
-  if (n > 1) then
-    (pow x (n-1)) * x
+let rec solve m n i =
+  if (m * 2 <= n) then
+    solve (m * 2) n (i+1)
   else
-    x;;
-
-let rec solve m n  =
-  if ((pow 2 m) <= n) then
-    solve (m+1) n
-  else
-    m-1;;
+    i;;
 
 let f3 n =
-  let m = 0 in
-    solve m n;;
+  solve 1 n 0;;
