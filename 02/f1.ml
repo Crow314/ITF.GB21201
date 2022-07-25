13b53b71426335baae32481b345c4cd9c28a298f
Crow314 <master@crow31415.net>
2022-07-26 06:27:38 +0900

    fix week02 f1

--- 02/f1.ml
+++ 02/f1.ml
@@ -1,13 +1,7 @@
-let max2 a b =
-  if a > b then
-    a
-  else
-    b;;
-
 let rec solve list1 max_val =
   match list1 with
   | [] -> max_val
-  | h::t -> solve t (max2 h max_val);;
+  | h::t -> solve t (Int.max h max_val);;
 
 let f1 list1 = 
-  solve list1 0;
+  solve list1 Int.min_int;
